3c852894b76d2ab7a5f0083308984ef8b27c087f
Kobi Cummings <kobicummings@gmail.com>
2019-09-16 11:04:20 -0700

    added event listener to submit button in doc.js

--- assets/javascript/doc.js
+++ assets/javascript/doc.js
@@ -52,6 +52,121 @@ $(document).ready(function () {
     $('select').formSelect();
 });
 //-------------------------------------------------------------------------
+$("#submit").on("click", function () {
+    var gender;
+    var location;
+    var specID;
+    var language;
+    var queryURL;
+    // var queryURL = "https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=" + specID + "&location=" + location + "&gender=" + gender + "&user_key=f89971ab4d8b480630c107682513f5b8";
+    if (specID) {
+        queryURL = "https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=" + specID + "&location=" + location + "&gender=" + gender + "&user_key=f89971ab4d8b480630c107682513f5b8";
+    } else {
+        queryURL = "https://api.betterdoctor.com/2016-03-01/doctors?location=" + location + "&gender=" + gender + "&user_key=f89971ab4d8b480630c107682513f5b8";
+    }
+
+
+
+    queryURL = "https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=sport-physical-therapist&location=ca-san-diego&gender=male&user_key=f89971ab4d8b480630c107682513f5b8";
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        var results = response.data; // array
+        for (var i = 0; i < results.length; i++) {
+            var doctorName;
+            var src = results[i].profile.image_url;
+            var doctorID = results[i].uid;
+            if (results[i].profile.middle_name) {
+                doctorName = results[i].profile.first_name + " " + results[i].profile.middle_name + " " + results[i].profile.last_name + "," + results[i].profile.title;
+            } else {
+                doctorName = results[i].profile.first_name + " " + results[i].profile.last_name + "," + results[i].profile.title;
+            }
+            var langArray1 = results[i].profile.languages;
+            var langArray2 = [];
+            for (var i = 0; i < langArray1.length; i++) {
+                langArray2.push(langArray1[i].name);
+            }
+            var languages = langArray2.toString();
+
+            var specArray = results[i].response.data.specialties;
+            var specArray2 = [];
+            for (var i = 0; i < specArray1.length; i++) {
+                specArray2.push(specArray1[i].name);
+            }
+            var specialties = specArray2.toString();
+            var address = results[i].practices[0].visit_address.street + " "
+            results[i].practices[0].visit_address.street2 + ","
+            results[i].practices[0].visit_address.city + ","
+            results[i].practices[0].visit_address.state + " "
+            results[i].practices[0].visit_address.zip;
+            var bio = results[i].profile.bio;
+            var newDiv = $("<div>");
+            newDiv.addClass("col l4 m12 s12");
+            newDiv.attr("id", "newDiv" + doctorID);
+            var newDiv1 = $("<div>");
+            newDiv1.addClass("card large");
+            // create new button for "Remove Favorite" for each favorite with id attribute set to doctorID from storage
+            var addToFavEle = $("<a>");
+            addToFavEle.addClass("btn-floating red btn tooltipped waves-effect waves-light remove-fav-button");
+            addToFavEle.attr("data-position", "right");
+            addToFavEle.attr("data-tooltip", "Remove from Favorites");
+            addToFavEle.attr("id", doctorID);
+            addToFavEle.html("<i class='material-icons'>delete</i>");
+            var newDiv11 = $("<div>");
+            newDiv11.addClass("card-image waves-effect waves-block waves-light");
+            newDiv11.html("<img class='activator' src='" + src + "'>");
+            var newDiv12 = $("<div>");
+            newDiv12.addClass("card-content");
+            var nameEle = $("<span>");
+            nameEle.addClass("card-title activator grey-text text-darken-4");
+            nameEle.html(doctorName + "<i class='material-icons right'>more_vert</i>");
+            var lanEle = $("<p>Languages: " + languages + "</p>");
+            var speEle = $("<p>Specialties: " + specialties + "</p>");
+            var addressEle = $("<p>Address: <a href='#'>" + address + "</a></p>");
+            newDiv12.append(nameEle, lanEle, speEle, addressEle);
+            var newDiv13 = $("<div>");
+            newDiv13.addClass("card-reveal");
+            var bioTitle = $("<span>");
+            bioTitle.addClass("card-title grey-text text-darken-4");
+            bioTitle.html("Bio:" + "<i class='material-icons right'>close</i>");
+            var bioContent = $("<p>" + bio + "</p>");
+            newDiv13.append(bioTitle, bioContent);
+            newDiv1.append(addToFavEle, newDiv11, newDiv12, newDiv13);
+            newDiv.append(newDiv1);
+            $("#doctor-cards").append(newDiv);
+        }
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Gender:
 //request URL:
 ///https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=f89971ab4d8b480630c107682513f5b8
