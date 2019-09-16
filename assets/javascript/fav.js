e5648eda8802292777929e2c7e4bd7dcb8f0c382
Sunny Tong <50933364+SunnyTong2019@users.noreply.github.com>
2019-09-15 22:39:11 -0700

    added event listener for add to favorite in fav.js to save data to localStorage

--- assets/javascript/fav.js
+++ assets/javascript/fav.js
@@ -1,67 +1,100 @@
 // event listener when click "Add to Favorites" button
-/*
 $(document).on("click", ".fav-button", function () {
 
-    // get the gifID from "Add to Favorites" button's id attribute
-    var gifID = $(this).attr("id");
-    var URL = "https://api.giphy.com/v1/gifs/" + gifID + "?api_key=nvpSYO2uu9JG3Hhm812QZ64TnGz1YZ3a";
+    // get the doctorID from "Add to Favorites" button's id attribute
+    var doctorID = $(this).attr("id");
+    var URL = "https://api.betterdoctor.com/2016-03-01/doctors/" + doctorID + "?user_key=f89971ab4d8b480630c107682513f5b8";
 
-    // send request to API to get the gif using gifID
+    // send request to API to get the doctor using doctorID
     $.ajax({
         url: URL,
         method: "GET"
     }).then(function (response) {
 
         // get the data you need from JSON response
-        var stillURL = response.data.images.fixed_height_still.url;
-        var rating = response.data.rating;
+        var src = response.data.profile.image_url;
+        var doctorName = response.data.profile.first_name + " " + response.data.profile.last_name + "," + response.data.profile.title;;
+
+        var langArray1 = response.data.profile.languages;
+        var langArray2 = [];
+        for (var i = 0; i < langArray1.length; i++) {
+            langArray2.push(langArray1[i].name);
+        }
+        var languages = langArray2.toString();
+
+        var specArray = response.data.specialties;
+        var specArray2 = [];
+        for (var i = 0; i < specArray1.length; i++) {
+            specArray2.push(specArray1[i].name);
+        }
+        var specialties = specArray2.toString();
+
+        var address = response.data.practices[0].visit_address.street + " "
+        response.data.practices[0].visit_address.street2 + ","
+        response.data.practices[0].visit_address.city + ","
+        response.data.practices[0].visit_address.state + " "
+        response.data.practices[0].visit_address.zip;
+
+        var bio = response.data.profile.bio;
 
         if (localStorage.getItem("favorites")) {  // if "favorites" key exists in localStorage
 
             var storage = JSON.parse(localStorage.getItem("favorites"));  // convert "favorites" key to array
 
-            for (var i = 0; i < storage.length; i++) { // if the gif is already in localStorage, exit function
-                if (storage[i].gifID === gifID) {
-                    // show a modal to alert user and hide the modal after 3 seconds
+            for (var i = 0; i < storage.length; i++) { // if the doctor is already in localStorage, exit function
+                if (storage[i].doctorID === doctorID) {
+                    // show a modal to alert user and hide the modal after 2 seconds
                     show_modal("modal2");
-                    setTimeout(function () { hide_modal("modal2"); }, 3000);
+                    setTimeout(function () { hide_modal("modal2"); }, 2000);
                     return false;
                 }
             }
 
-            // if function has not exit, which means the gif is not in localStorage, then add the gif to localStorage
+            // if function has not exit, which means the doctor is not in localStorage, then add the doctor to localStorage
             storage.push({
-                gifID: gifID,
-                url: stillURL,
-                rating: rating
+                doctorID: doctorID,
+                src: src,
+                doctorName: doctorName,
+                languages: languages,
+                specialties: specialties,
+                address: address,
+                bio: bio
             });
 
             localStorage.setItem('favorites', JSON.stringify(storage));
 
-            // show a modal to alert user and hide the modal after 3 seconds
+            // show a modal to alert user and hide the modal after 2 seconds
             show_modal("modal1");
-            setTimeout(function () { hide_modal("modal1"); }, 3000);
+            setTimeout(function () { hide_modal("modal1"); }, 2000);
 
         } else {  // if "favorites" key does NOT exist in localStorage
             var storage = [];
 
             storage.push({
-                gifID: gifID,
-                url: stillURL,
-                rating: rating
+                doctorID: doctorID,
+                src: src,
+                doctorName: doctorName,
+                languages: languages,
+                specialties: specialties,
+                address: address,
+                bio: bio
             });
 
             localStorage.setItem('favorites', JSON.stringify(storage));
 
-            // show a modal to alert user and hide the modal after 3 seconds
+            // show a modal to alert user and hide the modal after 2 seconds
             show_modal("modal1");
             setTimeout(function () { hide_modal("modal1"); }, 3000);
         }
     })
 });
 
-*/
 
+/*
+    $('#modal2').modal();
+    $('#modal2').modal('open');
+    setTimeout(function () { $('#modal2').modal('close'); } , 2000)   
+ */
 
 
 function show_modal(modalID) {
@@ -74,10 +107,10 @@ function hide_modal(modalID) {
     $('#' + modalID).modal('close');
 }
 
-localStorage.clear();
-var array=["a"];
-localStorage.setItem('favorites', JSON.stringify(array));
 
+//localStorage.clear();
+//var array = ["a"];
+//localStorage.setItem('favorites', JSON.stringify(array));
 
 
 if (localStorage.getItem("favorites")) {  // if "favorites" key exists in localStorage
@@ -94,7 +127,7 @@ if (localStorage.getItem("favorites")) {  // if "favorites" key exists in localS
             var languages = "English";
             var specialties = "Internal Medicine";
             var address = "7610 Carroll Ave Ste 280, Takoma Park, MD 20912";
-            var bio = "Specializing in internal medicine, Dr. Smith Ho, MD is one of the country's most highly ranked doctors. He currently treats patients in Takoma park,Maryland. Dr.Ho is licensed to treat patients in Maryland. Based on hiscredentials, network and experience, Dr.Ho is rated among the top 20 % of alldoctors in the United States. In addition, Dr.Ho has successfully passed abackground check including a medical license verification(active) and screening formalpractice history(none found).";
+            var bio = "Specializing in internal medicine, Dr. Smith Ho, MD is one of the country's most highly ranked doctors. He currently treats patients in Takoma park,Maryland. Dr.Ho is licensed to treat patients in Maryland. Based on his credentials, network and experience, Dr.Ho is rated among the top 20 % of all doctors in the United States. In addition, Dr.Ho has successfully passed abackground check including a medical license verification(active) and screening formalpractice history(none found).";
 
             var newDiv = $("<div>");
             newDiv.addClass("col l4 m12 s12");
@@ -103,7 +136,7 @@ if (localStorage.getItem("favorites")) {  // if "favorites" key exists in localS
             var newDiv1 = $("<div>");
             newDiv1.addClass("card large");
 
-            // create new button for "Remove Favorite" for each gif with id attribute set to gifID from storage
+            // create new button for "Remove Favorite" for each favorite with id attribute set to doctorID from storage
             var removeFavEle = $("<a>");
             removeFavEle.addClass("btn-floating red btn tooltipped waves-effect waves-light remove-fav-button");
             removeFavEle.attr("data-position", "right");
@@ -157,18 +190,18 @@ if (localStorage.getItem("favorites")) {  // if "favorites" key exists in localS
 // event listener when click "Remove Favorite" button
 $(document).on("click", ".remove-fav-button", function () {
 
-    // get the gifID that needs to be removed from the button's id attribute
-    var gifID = $(this).attr("id");
+    // get the doctorID that needs to be removed from the button's id attribute
+    var doctorID = $(this).attr("id");
 
     var storage = JSON.parse(localStorage.getItem("favorites"));
 
-    // filter out the gifID that needs to be removed and store it again to localStorage
-    var newStorage = storage.filter(item => item.gifID !== gifID);
+    // filter out the doctorID that needs to be removed and store it again to localStorage
+    var newStorage = storage.filter(item => item.doctorID !== doctorID);
 
     localStorage.setItem('favorites', JSON.stringify(newStorage));
 
-    // remove the gif div from the page
-    $("#newDiv" + gifID).remove();
+    // remove the doctor div from the page
+    $("#newDiv" + doctorID).remove();
 
     // if all favorites are removed, then display a text on the page
     // "favorites" key will still exist in localStorage but its value is just "[]"
@@ -179,7 +212,8 @@ $(document).on("click", ".remove-fav-button", function () {
 
 });
 
-// refresh this fav.html page whenever a favorite item is added to localStorage in case this page is open on a tab
+
+// refresh fav.html page whenever a favorite item is added to localStorage in case this page is open on a tab
 window.addEventListener("storage", function handler() {
     location.reload();
 });
